ac0fe340e15016e562b801e4fcb087a30eeaacfc
Philippe Groarke <philippe.groarke@gmail.com>
2019-11-21 22:11:53 -0500

    wip : Infrastructure.

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,6 +1,19 @@
 #include <alx_unique_ptr/alx_unique_ptr.hpp>
 #include <gtest/gtest.h>
 
+size_t allocations = 0;
+size_t deallocations = 0;
+void* operator new(std::size_t count) {
+	++allocations;
+	return malloc(count);
+}
+
+void operator delete(void* p) {
+	--deallocations;
+	free(p);
+}
+
+
 namespace {
 TEST(alx_unique_ptr, basics) {
 	EXPECT_TRUE(true);
